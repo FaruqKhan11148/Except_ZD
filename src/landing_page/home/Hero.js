5deb1e923c625de9cea483934a183775035cfc4d
FaruqKhan11148 <faruqkhanshired111@gmail.com>
2025-04-05 19:54:13 +0530

    Project is completed now have to deploy it

--- src/landing_page/home/Hero.js
+++ src/landing_page/home/Hero.js
@@ -8,7 +8,7 @@ function Hero() {
                     <img src="media/images/homeHero.png" alt="heroImg" className='mb-5'/>
                     <h1 className='mt-5'>Invest in everything</h1>
                     <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-                    <button className='p-2 btn btn-primary fs-5' style={{width:"20%", margin:"0 auto"}}>Sign up for free</button>
+                    <button name='submitButton' className='p-2 btn btn-primary fs-5' style={{width:"20%", margin:"0 auto"}}>Sign up for free</button>
                 </div>
             </div>
         </>
