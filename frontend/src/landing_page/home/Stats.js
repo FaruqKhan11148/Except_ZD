644321c46a65fe8b9f668b305394d0e6c2dcb8d7
FaruqKhan11148 <faruqkhanshired111@gmail.com>
2025-07-17 00:46:32 +0530

    updated

--- frontend/src/landing_page/home/Stats.js
+++ frontend/src/landing_page/home/Stats.js
@@ -17,13 +17,13 @@ function Stats() {
                         <p className='text-muted'>Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to your needs.</p>
                         
                         <h4 className='fs-4'>Do better with money</h4>
-                        <p className='text-muted'>With initiatives like <a style={{textDecoration:"none"}} href="">Nudge</a> and <a style={{textDecoration:"none"}} href="">Kill Switch</a>, we don't just facilitate transactions, but actively help you do better with your money.</p>
+                        <p className='text-muted'>With initiatives like <a style={{textDecoration:"none"}} href="javascript:void(0)">Nudge</a> and <a style={{textDecoration:"none"}} href="javascript:void(0)">Kill Switch</a>, we don't just facilitate transactions, but actively help you do better with your money.</p>
                     </div>
                     <div className="col-7 p-5">
                         <img style={{width:"80%"}} src="media/images/ecosystem.png" alt="awardImg" />
                         <div className='text-center'>
-                            <a href="" className='mx-3' style={{textDecoration:"none"}} >Explore our products <i style={{color:"blue"}} class="fa-solid fa-arrow-right"></i></a>
-                            <a href="" style={{textDecoration:"none"}} >Try Kite demo <i style={{color:"blue"}} class="fa-solid fa-arrow-right"></i></a>
+                            <a href="javascript:void(0)" className='mx-3' style={{textDecoration:"none"}} >Explore our products <i style={{color:"blue"}} class="fa-solid fa-arrow-right"></i></a>
+                            <a href="javascript:void(0)" style={{textDecoration:"none"}} >Try Kite demo <i style={{color:"blue"}} class="fa-solid fa-arrow-right"></i></a>
                         </div>
                     </div>
                     <img style={{width:"55%", margin:"0 auto"}} src="media/images/pressLogos.png" alt="pressImg" />
