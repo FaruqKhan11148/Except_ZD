8f70808695bc26b856aa8a86258a7eacdad1f054
FaruqKhan11148 <faruqkhanshired111@gmail.com>
2025-07-16 16:30:48 +0530

    signup page setup done

--- src/landing_page/signup/Signup.js
+++ src/landing_page/signup/Signup.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import OpenAccFree from './OpenAccFree';
+import SignupNow from './SignupNow';
+import Investment from './Investment';
+import Steps from './Steps';
+import UnbeatenPricing from './UnbeatenPricing';
+import Explore from './Explore';
+import Faqs from './Faqs';
+import OpenAcc from './OpenAcc';
+
 function Signup() {
     return ( 
-        <h1>Signup</h1>
+        <>
+        <OpenAccFree/>
+        <SignupNow/>
+        <Investment/>
+        <Steps/>
+        <UnbeatenPricing/>
+        <Explore/>
+        <Faqs/>
+        <OpenAcc/>
+        </>
      );
 }
 
