c2fc14796ac1b84c091dff3e33d4f3ecb9c86123
Faruq Khan <150416913+FaruqKhan11148@users.noreply.github.com>
2025-10-21 11:10:26 +0530

    updation in OrderesSchema

--- backend/model/OrdersModel.js
+++ backend/model/OrdersModel.js
@@ -3,5 +3,6 @@ const {model}= require("mongoose");
 
 const {OrdersSchema}=require("../schemas/OrdersSchema");
 
+const OrdersModel= new model("order", OrdersSchema);
 
 module.exports={OrdersModel};
